cc1e3575d3c8e0445b1ec273018937d75dff76c0
bilded <cjsfhdurwjd3382@nacer.com>
2024-02-22 00:53:10 +0900

    oneDay

---
+++ chap01/src/chap06/VerDemo.java
@@ -0,0 +1,13 @@
+package chap06;
+
+public class VerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+
+		System.out.println(6 / 2.0);
+
+	}
+
+}
